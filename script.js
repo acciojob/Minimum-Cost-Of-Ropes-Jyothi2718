305e9c924901967c77c5df4b4422fc21c43f5d39
Jyothirmai <123802567+Jyothi2718@users.noreply.github.com>
2023-05-18 15:21:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
 function calculateMinCost() {
   //your code here
+  // Get the input value
+  var input = document.getElementById("input").value;
+
+  // Split the input by commas and convert the values to numbers
+  var ropes = input.split(",").map(Number);
+
+  // Sort the ropes in ascending order
+  ropes.sort(function(a, b) {
+    return a - b;
+  });
+
+  // Initialize the total cost variable
+  var totalCost = 0;
+	// Iterate through the ropes
+  while (ropes.length > 1) {
+    // Take the two smallest ropes
+    var smallestRope1 = ropes.shift();
+    var smallestRope2 = ropes.shift();
+
+    // Calculate the cost of connecting the ropes
+    var cost = smallestRope1 + smallestRope2;
+
+    // Add the cost to the total
+    totalCost += cost;
+
+    // Insert the connected rope back into the sorted array
+    // to maintain the order
+    var index = 0;
+    while (index < ropes.length && ropes[index] < cost) {
+      index++;
+    }
+    ropes.splice(index, 0, cost);
+  }
+	// Display the result
+  document.getElementById("result").innerHTML = totalCost;
+}
+
   
-  
-  
-}  
+    
