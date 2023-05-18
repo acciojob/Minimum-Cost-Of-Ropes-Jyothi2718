23fd4fea6baa26126c400da9245883d8fa4b2cee
Jyothirmai <123802567+Jyothi2718@users.noreply.github.com>
2023-05-18 15:26:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,41 +1,46 @@
 function calculateMinCost() {
-  //your code here
   // Get the input value
-  var input = document.getElementById("input").value;
-
-  // Split the input by commas and convert the values to numbers
-  var ropes = input.split(",").map(Number);
-
-  // Sort the ropes in ascending order
-  ropes.sort(function(a, b) {
+  var input = document.getElementById('input').value;
+  
+  // Convert the input string to an array of integers
+  var arr = input.split(',').map(function(item) {
+    return parseInt(item, 10);
+  });
+  
+  // Sort the array in non-decreasing order
+  arr.sort(function(a, b) {
     return a - b;
   });
-
-  // Initialize the total cost variable
+  
+  // Variable to store the total cost
   var totalCost = 0;
-	// Iterate through the ropes
-  while (ropes.length > 1) {
-    // Take the two smallest ropes
-    var smallestRope1 = ropes.shift();
-    var smallestRope2 = ropes.shift();
-
+  
+  // Iterate over the array and add the two smallest ropes at each step
+  while (arr.length > 1) {
+    // Get the two smallest ropes
+    var smallest1 = arr.shift();
+    var smallest2 = arr.shift();
+    
     // Calculate the cost of connecting the ropes
-    var cost = smallestRope1 + smallestRope2;
-
-    // Add the cost to the total
+    var cost = smallest1 + smallest2;
+    
+    // Add the cost to the total cost
     totalCost += cost;
-
-    // Insert the connected rope back into the sorted array
-    // to maintain the order
-    var index = 0;
-    while (index < ropes.length && ropes[index] < cost) {
-      index++;
-    }
-    ropes.splice(index, 0, cost);
+    
+    // Add the connected rope to the array
+    arr.push(cost);
+    
+    // Sort the array again
+    arr.sort(function(a, b) {
+      return a - b;
+    });
   }
-	// Display the result
-  document.getElementById("result").innerHTML = totalCost;
+  
+  // Display the minimum cost in the result div
+  var resultDiv = document.getElementById('result');
+  resultDiv.innerHTML = totalCost;
 }
+
 
   
     
